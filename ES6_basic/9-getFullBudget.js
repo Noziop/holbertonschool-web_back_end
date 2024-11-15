96d3a4ce9dcc8f4f686a268c9095637661f93e0e
Noziop <contact@fassih.ch>
2024-11-15 20:33:06 +0100

    ADD : project ES6

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,13 +1,13 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) { // Plus besoin de 'function'
+    getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros(income) { // Plus besoin de 'function'
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
